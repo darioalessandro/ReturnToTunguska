4d5c870d564ac51ce7956ddcb96ac4a6ffbf7175
darioalessandro <darioalessandrolencina@gmail.com>
2015-09-03 02:41:31 -0400

    Added wireframes

---
+++ app/controllers/PortfolioController.scala
@@ -0,0 +1,8 @@
+package controllers
+
+/**
+ * Created by darioalessandro on 9/3/15.
+ */
+class PortfolioController {
+
+}
---
+++ app/controllers/PostViewerController.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api.mvc.Controller
+
+/**
+ * Created by darioalessandro on 9/3/15.
+ */
+
+class PostViewerController extends Controller {
+
+}
